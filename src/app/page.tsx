d20a95e5999a2a931be404f79a3165b008cd3c8c
Franco Serjen <francoserjen100@gmail.com>
2025-11-26 13:56:57 -0300

    Work in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import Hero from "@/components/Hero";
 import Header from "../components/Header";
 // import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <div className="relative h-screen bg-img-header-hero flex flex-col">
+        <div className="absolute inset-0 bg-[#001623cc]/80" aria-hidden="true" />
+        <div className="relative z-10 flex flex-col flex-1">
+          <Header />
+          <Hero />
+        </div>
+      </div>
     </>
   );
 }
